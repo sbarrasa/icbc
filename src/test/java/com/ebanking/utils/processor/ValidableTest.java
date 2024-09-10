7ab85f568b654dafdceb39085fe7a59b7256d2cc
Sbarrasa <zaiper2000@gmail.com>
2024-09-10 19:38:41 -0300

    refactor types

--- src/test/java/com/ebanking/utils/processor/ValidableTest.java
+++ src/test/java/com/ebanking/utils/processor/ValidableTest.java
@@ -3,6 +3,7 @@ package com.ebanking.utils.processor;
 import org.junit.jupiter.api.Test;
 
 import java.util.Objects;
+import java.util.function.Predicate;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -25,5 +26,18 @@ class ValidableTest {
 
   }
 
+  @Test
+  void validateLambda(){
+    Predicate<Integer> isPositive = value -> value != null && value > 0;
+
+    Validable<Integer> validable = (value) -> {
+        if(!isPositive.test(value)) throw new Exception("EL valor debe ser mayor que 0");
+    };
+
+    assertDoesNotThrow(() -> validable.validate(1234));
+    assertThrows(Exception.class, () -> validable.validate(null));
+    assertThrows(Exception.class, () -> validable.validate(-1));
+
+  }
 
 }
