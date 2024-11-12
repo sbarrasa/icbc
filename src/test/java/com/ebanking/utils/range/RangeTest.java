3853e54bbf58d3d35c4b4829479f48b2fac11c8f
Sbarrasa <zaiper2000@gmail.com>
2024-11-12 01:34:10 -0300

    Simplify Range

--- src/test/java/com/ebanking/utils/range/RangeTest.java
+++ src/test/java/com/ebanking/utils/range/RangeTest.java
@@ -2,22 +2,9 @@ package com.ebanking.utils.range;
 
 import org.junit.jupiter.api.Test;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class RangeTest {
-  @Test
-  void rangeEmptyTest() throws Exception {
-    var range = new Range<Date>();
-    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-
-    range.setMin(formatter.parse("07/06/1974"));
-    range.setMax(new Date());
-
-    assertTrue(range.contains(new Date()));
-  }
 
   @Test
   void rangeCreateTest() {
